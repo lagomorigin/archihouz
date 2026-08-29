1ac775de9aa06dbbfecbde34d7002f2469185fd2
lagomorigin <lagomorigin@users.noreply.github.com>
2026-08-29 05:50:35 +0000

    Dựng LP sơn nhà, sơn sửa nhà và chống thấm tường.

---
+++ src/site.ts
@@ -0,0 +1,4 @@
+/** Số tạm Archihouz (Zen chốt 29/08/2026). Không lấy số khác. */
+export const SO_TAM = "0932196959";
+export const ZALO_HREF = "https://zalo.me/0932196959";
+export const PHONE_HREF = "tel:0932196959";
